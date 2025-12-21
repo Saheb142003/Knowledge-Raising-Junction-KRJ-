a8df63b071b40c6caccf968c826f0698a6ae4f75
AnbCrafts <anubhaw22052003@gmail.com>
2025-12-21 09:53:29 +0530

    Final Schema layout  done

--- server/src/Schema/Board/Board.js
+++
@@ -1,5 +0,0 @@
-const BoardSchema = new mongoose.Schema({
-  name: String,
-});
-
-export const Board = mongoose.model("Board", BoardSchema);
--- server/src/Schema/Shift/Shift.Students.js
+++
@@ -1,8 +0,0 @@
-const ShiftSchema = new mongoose.Schema({
-  name: String, // Morning / Evening
-  startTime: String,
-  endTime: String,
-  breakDuration: String,
-});
-
-export const Shift = mongoose.model("Shift", ShiftSchema);
